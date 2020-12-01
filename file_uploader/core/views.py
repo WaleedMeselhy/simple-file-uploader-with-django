7fba415bd48c157296fa61a8fe5cf1b7201a2066
Waleed Meselhy <waleed_meselhy@eng.asu.edu.eg>
2020-12-01 22:55:13 +0200

    fix duplicated results

--- file_uploader/core/views.py
+++ file_uploader/core/views.py
@@ -19,9 +19,10 @@ def index(request):
         form = SearchForm(request.POST)
         if form.is_valid():
             files_from_content = []
-            files_saved = list(
-                File.objects.filter(name__icontains=form.cleaned_data["file_name"])
-            )
+            files_saved = File.objects.filter(
+                name__icontains=form.cleaned_data["file_name"]
+            ).values_list("id", flat=True)
+            files_from_content.extend(files_saved)
             results = es.search(
                 index="",
                 body={
@@ -29,16 +30,15 @@ def index(request):
                 },
             )
             if results["hits"]["total"]["value"] > 0:
-                files_ids = []
                 for result in results["hits"]["hits"]:
-                    files_ids.append(result["_source"]["file_id"])
+                    files_from_content.append(result["_source"]["file_id"])
 
-                files_from_content = list(File.objects.filter(id__in=files_ids))
+            files = list(File.objects.filter(id__in=list(set(files_from_content))))
 
             return render(
                 request,
                 "index.html",
-                {"show_files": True, "files_saved": files_from_content + files_saved},
+                {"show_files": True, "files_saved": files},
             )
     files_saved = list(File.objects.all())
     return render(
